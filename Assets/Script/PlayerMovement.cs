bff060c0bb1cc51dc3f038c5e2c47b4d4e46c7eb
PunnawitAck3003 <punnawitack@gmail.com>
2024-03-20 21:48:23 +0700

    FixInitia_MagicCircle

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
+
+/*public class PlayerMovement : MonoBehaviour
+{
     public float moveSpeed = 30f; // Adjust this to change player speed
 
     Rigidbody2D rb;
@@ -28,4 +64,4 @@ public class PlayerMovement : MonoBehaviour
         // Apply movement to the Rigidbody2D
         rb.MovePosition(rb.position + movement);
     }
-}
+}*/
